12b8753266e1e859f5662f92e5c6f0ece1d71388
Chris D <chris@venngo.com>
2021-06-19 18:10:54 -0400

    example

---
+++ example/yt_playlist.dart
@@ -0,0 +1,24 @@
+import 'package:yt/yt.dart';
+
+void main() async {
+  var yt = Yt.withOAuth(OAuthCredentials.fromYaml('example/youtube.yaml'));
+
+  // var yt = Yt.withKey('[youtube api key]');
+
+  var playlists = await yt.playlist;
+
+  // var playlistResponse = await playlists.list(mine: true);
+
+  var playlistResponse = await playlists.list(
+      channelId: '[youtube channle id]', maxResults: 25);
+
+  playlistResponse.items.forEach((playlist) => print(
+      'title: ${playlist.snippet?.title}, ${playlist.snippet?.thumbnails.thumbnailsDefault.url}'));
+
+  // playlistResponse = await playlists.list(
+  //     mine: true, pageToken: playlistResponse.nextPageToken);
+
+  // playlistResponse.items
+  //     .forEach((playlist) => print('${playlist.snippet?.title}'));
+}
+
